1b4432c6c40f0efb5b73c1498e839eff03c0a2f1
Yevhen Mazur <yevhen.mazur@pm.me>
2024-10-04 18:02:38 +0200

    Socket-server up and running

--- main.py
+++ main.py
@@ -16,20 +16,26 @@ uri = "mongodb://mongodb:27017"
 
 HTTPServer_Port = 3000
 HTTPDocs = './httpdoc'
-UDP_IP = '127.0.0.1'
-UDP_PORT = 5000
-logging.basicConfig(level=logging.INFO)
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5000
 
 
 class HttpGetHandler(BaseHTTPRequestHandler):
-    '''Базовий веб-сервер'''
+    '''Вбудований у додаток веб-сервер'''
 
     def do_POST(self):
+        '''Обробник POST запитів. Приймає дані з веб-форми і відправляє у SocketServer для обробки'''
         logging.info("POST request received: %s", self.path)
-        pass
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        self.send_response(302)
+        self.send_header('Location', '/message.html')
+        self.end_headers()
+        
+        send_data_to_socket(data)
 
 
     def do_GET(self):
+        '''Обробник GET запитів. Віддає сторінку, відповідну до URL'''
         logging.info("GET request received: %s", self.path)
         pr_url = urllib.parse.urlparse(self.path)
         if pr_url.path == '/':
@@ -43,6 +49,7 @@ class HttpGetHandler(BaseHTTPRequestHandler):
                 self.send_error(404, "File Not Found")
 
     def send_html_file(self, filename, status_code=200):
+        '''Віддає файл, чи сторінку 404'''
         try:
             with open(filename, 'rb') as f:
                 self.send_response(status_code)
@@ -53,6 +60,7 @@ class HttpGetHandler(BaseHTTPRequestHandler):
             self.send_html_file('error.html', 404)
 
     def send_static(self):
+        '''Віддає статичні ресурси чи повідомляє про помилку'''
         file_path = pathlib.Path(self.path[1:])
         self.send_response(200)
         mt = mimetypes.guess_type(file_path)
@@ -67,6 +75,7 @@ class HttpGetHandler(BaseHTTPRequestHandler):
 
 
 def run_http_server(server_class=HTTPServer, handler_class=HttpGetHandler):
+    '''Запускає вбудований веб-сервер і обробляє помилки у ньому'''
     server_address = ('0.0.0.0', HTTPServer_Port)
     logging.info("Changing working directory to %s", HTTPDocs)
     os.chdir(HTTPDocs)
@@ -85,9 +94,15 @@ def run_http_server(server_class=HTTPServer, handler_class=HttpGetHandler):
 
 
 def send_data_to_socket(data):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server = UDP_IP, UDP_PORT
-    #TODO Дописати відправку даних
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((TCP_IP, TCP_PORT))
+            s.sendall(data) 
+            # print(f'Data sent: {data}')
+        except ConnectionRefusedError:
+            logging.error("Unable to connect to the server")
+
 
 def save_data(data):
     client = MongoClient(uri, server_api=ServerApi("1"))
@@ -102,23 +117,37 @@ def save_data(data):
     }
     """
     # Ключ "date" кожного повідомлення — це час отримання повідомлення: datetime.now()
-    data_parse = urllib.parse.unquote_plus(data.decode())
     
 
-def run_socket_server(ip, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server = ip, port
-    sock.bind(server)
+def run_socket_server(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((host, port))
+        s.listen(1)
+        logging.info("SocketServer started")
+
+        conn, addr = s.accept()
+        logging.info("SocketServer started")
+
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                string_data = (data.decode())
+                decoded_data = urllib.parse.unquote_plus(string_data)
+                data_dict = {key: value for key, value in [el.split('=') for el in decoded_data.split('&')]}
+
+                print(f"Прийнято на сокет-сервері: {data_dict}")
     #TODO Дописати логіку прийняття даних та їх збереження в БД
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(threadName)s %(message)s')
 
-    #TODO Зробити два процеса для кожного з серверів
-    # http_server_process = Process()
-    # http_server_process.start()
+    # Запускаю веб- і сокет- сервери в окремих процесах
+    http_server_process = Process(target=run_http_server)
+    http_server_process.start()
 
-    # socket_server_process = Process()
-    # socket_server_process.start()
-    run_http_server()
+    socket_server_process = Process(target=run_socket_server, args=(TCP_IP, TCP_PORT))
+    socket_server_process.start()
     
